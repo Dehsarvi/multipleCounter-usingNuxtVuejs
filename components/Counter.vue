af45e330cfcd848dca77f3bf8e2d9593c405770a
dehsarvi <atousa.dehsarvi@gmail.com>
2020-07-30 02:48:24 +0430

    first commit

---
+++ components/Counter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card-group d-inline-block">
+    <div class="card shadow m-2 bg-white rounded border-warning">
+      <div class="card-body text-center">
+        <h4 class="card-title text-warning">{{ innerStep }} Step</h4>
+        <h1 class="card-text text-center">{{ fixedInnerValue }}</h1>
+        <button
+          class="btn btn-outline-warning "
+          @click="run"
+          @keydown.enter="run"
+        >
+          run!
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "counter",
+  data() {
+    return {
+      innerStep: this.step,
+      innerValue: 0
+    };
+  },
+  props: {
+    //input of step from parent index.vue
+    step: Number
+  },
+  computed: {
+    //only accept float number with one decimal digit
+    fixedInnerValue() {
+      // https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
+      return this.innerValue.toFixed(1);
+    }
+  },
+  methods: {
+    run() {
+      this.innerValue += this.innerStep;
+    }
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <h1 class="text-center my-5 text-warning">Multiple Counters</h1>
+    <div class="row justify-content-center my-5">
+      <input
+        class="w-25 mx-1 border border-top-0 border-right-0 border-left-0 border-warning"
+        type="number"
+        v-model.number="step"
+        placeholder="Step"
+        step="0.1"
+        autofocus
+        @keyup.enter="addCounter"
+      />
+      <button class="mx-1 btn btn-outline-warning" @click="addCounter">
+        Add counter
+      </button>
+    </div>
+    <div class="row">
+      <counter
+        v-for="counter in counters"
+        :key="counter.id"
+        :step="counter.step"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      step: null,
+      counters: []
+    };
+  },
+  methods: {
+    // addCounter first check if the input is acceptable then push it into the counter card
+    addCounter() {
+      if (/^[\-\+]?\d*\.?\d?$/.test(this.step)) {
+        this.counters.push({
+          id: this.counters.length,
+          step: this.step
+        });
+        this.step = null;
+      } else {
+        alert(
+          "Please enter a negative or positive integer or a float with one decimal."
+        );
+      }
+    }
+  }
+};
+</script>
